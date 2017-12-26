76b84d37889ac440c1d8dab2b5361251b3b34228
Rino Ridlo Julianto <rinoridlojulianto@gmail.com>
2017-12-26 21:26:42 +0700

    Commit pertama

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo "<table>";
+    echo "<tr>";
+    echo "<td>Nama</td>";
+    echo "<td>:</td>";
+    echo "<td>Rino Ridlo Julianto</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>NIM</td>";
+    echo "<td>:</td>";
+    echo "<td>155410116</td>";
+    echo "</tr>";
+    echo "<tr>";
+    echo "<td>Jurusan</td>";
+    echo "<td>:</td>";
+    echo "<td>Teknik Informatika</td>";
+    echo "</tr>";
+    echo "</table>";
